7d31d336418606946d73996b6bb087eff5a7329c
Brian Scherner <brianscherner@gmail.com>
2023-11-13 11:03:45 -0800

    Add home Index page and Store Index page.

--- GuitarVendor/Controllers/HomeController.cs
+++ GuitarVendor/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
+using GuitarVendor.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GuitarVendor.Controllers
 {
   public class HomeController : Controller
   {
+
+    // private readonly GuitarVendorContext _db;
+    // public HomeController(GuitarVendorContext db)
+    // {
+    //   _db = db;
+    // }
 
     [HttpGet("/")]
     public ActionResult Index()
