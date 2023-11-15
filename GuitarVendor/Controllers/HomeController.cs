c84a4fd64ab29105962daefb5115da40bc8956af
Brian Scherner <brianscherner@gmail.com>
2023-11-15 13:36:49 -0800

    Add splash page and update routes in model views.

--- GuitarVendor/Controllers/HomeController.cs
+++ GuitarVendor/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 using GuitarVendor.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GuitarVendor.Controllers
 {
   public class HomeController : Controller
   {
 
-    // private readonly GuitarVendorContext _db;
-    // public HomeController(GuitarVendorContext db)
-    // {
-    //   _db = db;
-    // }
+    private readonly GuitarVendorContext _db;
+    public HomeController(GuitarVendorContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Store[] stores = _db.Stores.ToArray();
+      Guitar[] guitars = _db.Guitars.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("stores", stores);
+      model.Add("guitars", guitars);
+      return View(model);
     }
 
   }
